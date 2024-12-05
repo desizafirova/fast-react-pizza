999f79c868e0615ebf1a145c505b18d999fe0d74
DesiZafirova <desszafirowa@gmail.com>
2024-12-05 10:46:10 +0200

    styling borders and spacing

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,17 +1,16 @@
-import { Outlet, useNavigation } from 'react-router-dom';
-import CartOverview from '../features/cart/CartOverview';
-import Header from './Header';
-import Loader from './Loader';
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
   return (
     <div className="layout">
       {isLoading && <Loader />}
       <Header />
       <main>
-        <h1>Content</h1>
         <Outlet />
       </main>
       <CartOverview />
