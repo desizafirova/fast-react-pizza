aa34283baf92fbea013808556e1c189b50a83bde
DesiZafirova <desszafirowa@gmail.com>
2024-12-06 14:04:28 +0200

    creating cartSlice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  //   cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: 'Mediterranean',
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {},
+    deleteItem(state, action) {},
+    increaseItemQuantity(state, action) {},
+    decreaseItemQuantity(state, action) {},
+    clearCart(state, action) {},
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
